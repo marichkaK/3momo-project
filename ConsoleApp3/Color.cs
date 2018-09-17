418f4e63b1d1aeee5039d96991a47c69ce84fbb2
Mariia Kunyk <maria199881@gmail.com>
2018-09-17 14:27:29 +0300

    Add project files.

---
+++ ConsoleApp3/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Color
+    {
+        public String color { get; set; }
+
+        public Color(String color)
+        {
+            this.color = color;
+        }
+
+        public String getColor()
+        {
+            return color;
+        }
+    }
+}
